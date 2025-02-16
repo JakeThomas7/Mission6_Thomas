2f658ad7ff81ea0319ed89ae45abcf87e1017156
JakeThomas7 <131414634+JakeThomas7@users.noreply.github.com>
2025-02-15 20:12:21 -0700

    Adding a few more comments

--- Mission6_Thomas/Models/MovieContext.cs
+++ Mission6_Thomas/Models/MovieContext.cs
@@ -2,13 +2,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Mission6_Thomas.Models;
 
-// This is the Movie Context document for setting up the database
+// This is the MovieContext class, responsible for setting up and interacting with the database.
 public class MovieContext : DbContext
 {
+    // Constructor that takes DbContextOptions to pass into the base class for configuring the context
     public MovieContext(DbContextOptions<MovieContext> options) : base(options)
     {
-        
+        // The base constructor initializes the DbContext with the provided options.
+
     }
-    
+    // DbSet representing the "Applications" table in the database. This will hold the application data.
     public DbSet<Application> Applications { get; set; }
 }
